9b19d8c0b988afd62ed48d3bb04a7094801578cd
robert <robert.fraser@uqconnect.edu.au>
2020-01-23 17:04:43 +1000

    Pipe updaters for Polar charts

--- src/Chartjs/DataSets/Polar.elm
+++ src/Chartjs/DataSets/Polar.elm
@@ -2,6 +2,13 @@ module Chartjs.DataSets.Polar exposing
     ( DataSet
     , defaultPolarFromData
     , defaultPolarFromLabel
+    , setBackgroundColor
+    , setBorderAlign
+    , setBorderColor
+    , setBorderWidth
+    , setHoverBackgroundColor
+    , setHoverBorderColor
+    , setHoverBorderWidth
     )
 
 import Chartjs.Common as Common
@@ -38,3 +45,43 @@ defaultPolarFromData label data =
     , hoverBorderColor = Nothing
     , hoverBorderWidth = Nothing
     }
+
+
+setData : List Float -> DataSet -> DataSet
+setData data dataset =
+    { dataset | data = data }
+
+
+setBackgroundColor : Common.PointProperty Color -> DataSet -> DataSet
+setBackgroundColor color dataset =
+    { dataset | backgroundColor = Just color }
+
+
+setBorderAlign : String -> DataSet -> DataSet
+setBorderAlign align dataset =
+    { dataset | borderAlign = Just align }
+
+
+setBorderColor : Common.PointProperty Color -> DataSet -> DataSet
+setBorderColor color dataset =
+    { dataset | borderColor = Just color }
+
+
+setBorderWidth : Common.PointProperty Float -> DataSet -> DataSet
+setBorderWidth width dataset =
+    { dataset | borderWidth = Just width }
+
+
+setHoverBackgroundColor : Common.PointProperty Color -> DataSet -> DataSet
+setHoverBackgroundColor color dataset =
+    { dataset | hoverBackgroundColor = Just color }
+
+
+setHoverBorderColor : Common.PointProperty Color -> DataSet -> DataSet
+setHoverBorderColor color dataset =
+    { dataset | hoverBorderColor = Just color }
+
+
+setHoverBorderWidth : Common.PointProperty Float -> DataSet -> DataSet
+setHoverBorderWidth width dataset =
+    { dataset | hoverBorderWidth = Just width }
