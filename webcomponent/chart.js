c2215ab8fdaeeb88ab13ac279e060d22f5aeaed3
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-01-23 20:47:39 +1000

    Format web component

--- webcomponent/chart.js
+++ webcomponent/chart.js
@@ -1,36 +1,37 @@
 /*
- This is a major stripped down version of the original web component
- This one properly creates the chart & updates the chart property settings
- However there is no support for setting callback functions at this stage
+ This is a simple implementation of a ChartJs web component
+ This should handle resizing, data updates, and options as expected
+
+ For more advanced functionality, consider using native JS instead of Elm
 */
-import Chart from 'chart.js';
+import Chart from 'chart.js'
 
 class ChartjsChart extends window.HTMLElement {
-  constructor() {
+  constructor () {
     const self = super()
     this._chartConfig = {}
     return self
   }
 
-  connectedCallback() {
-    let canvas = document.createElement("canvas")
+  connectedCallback () {
+    const canvas = document.createElement('canvas')
     this._canvas = canvas
     this.appendChild(canvas)
 
-    const ctx = canvas.getContext("2d")
-    this.style.display = "block"
+    const ctx = canvas.getContext('2d')
+    this.style.display = 'block'
     this._chart = new Chart(ctx, this._chartConfig)
   }
 
-  set chartConfig(newValue) {
+  set chartConfig (newValue) {
     this._chartConfig = newValue
 
-    if(this._chart) {
+    if (this._chart) {
       const newDatasets = newValue.data.datasets
       const oldDatasets = this._chart.data.datasets
 
       // Update all datasets
-      for(let i=0; i < newDatasets.length; i++) {
+      for (let i = 0; i < newDatasets.length; i++) {
         // Copying the meta will keep the animations between them smooth
         newDatasets[i]._meta = oldDatasets[i]._meta
         oldDatasets[i] = newDatasets[i]
