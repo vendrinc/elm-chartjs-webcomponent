f89bb306bcae3db3438ed16633a5934dea479ed8
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-05-31 16:23:08 +1000

    Update examples

---
+++ examples/src/MixedChart.elm
@@ -0,0 +1,88 @@
+module MixedChart exposing (main)
+
+import Browser
+import Chartjs.Chart as Chart
+import Chartjs.Common as ChartCommon
+import Chartjs.Data as ChartData
+import Chartjs.DataSets.Bar as BarData
+import Chartjs.DataSets.Line as LineData
+import Color
+import Html exposing (Html, div)
+
+type alias Model =
+    { data1 : List Float
+    , data2 : List Float
+    , data3 : List Float
+    , labels : List String
+    }
+
+init : Model
+init =
+    { data1 = [10, 20, 20, 10, 15]
+    , data2 = [5, 15, 10, 10, 15]
+    , data3 = [10, 15, 12, 12, 18]
+    , labels = ["Jan", "Feb", "Mar", "Apr", "May"]
+    }
+
+
+
+{-| Build a Chartjs data object from our model
+First we need a Data container which has some basic chart information
+Then we need to make the dataset for our bar chart
+This can be done easily using pipe operators instead of record update syntax
+-}
+data : Model -> ChartData.Data
+data model =
+    let
+        dataset1 =
+            BarData.defaultBarFromData "Bar One" model.data1
+                |> BarData.setBackgroundColor (ChartCommon.All Color.red)
+                |> ChartData.BarData
+
+        dataset2 =
+            BarData.defaultBarFromData "Bar Two" model.data2
+                |> BarData.setBackgroundColor (ChartCommon.All Color.blue)
+                |> ChartData.BarData
+
+        dataset3 =
+            LineData.defaultLineFromData "The Line" model.data3
+                |> LineData.setBorderColor (ChartCommon.All Color.green)
+                |> LineData.setBackgroundColor (ChartCommon.All Color.green)
+                |> ChartData.LineData
+    in
+    ChartData.dataFromLabels model.labels
+        |> ChartData.setDatasets [dataset3, dataset1, dataset2]
+
+{-| Build the full chart configuration from our model
+Right now, we're only setting custom data up
+When we have more complicated chart options, we'd add it to the config here
+-}
+chartConfig : Model -> Chart.Chart
+chartConfig model =
+    Chart.defaultChart Chart.Bar
+        |> Chart.setData (data model)
+
+
+{-| Display the chart using the chart config
+If we wanted to resize the chart or add other attributes, we can do that here
+-}
+view : Model -> Html msg
+view model =
+    Chart.chart [] (chartConfig model)
+
+
+{-| There's nothing to update in this example, so this is a dummy update function
+-}
+update : Model -> msg -> Model
+update model _ =
+    model
+
+
+{-| Simple browser sandbox to run this example
+-}
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
---
+++ examples/src/PieChartWithOptions.elm
@@ -0,0 +1,101 @@
+module PieChartWithOptions exposing (main)
+
+import Browser
+import Chartjs.Chart as Chart
+import Chartjs.Common as ChartCommon
+import Chartjs.Data as ChartData
+import Chartjs.Options as ChartOptions
+import Chartjs.Options.Legend as ChartLegend
+import Chartjs.Options.Title as ChartTitle
+import Chartjs.Options.Font as ChartFont
+import Chartjs.DataSets.DoughnutAndPie as PieData
+import Color
+import Html exposing (Html, div)
+
+
+{-| Our model keeps track of some basic chart information
+This isn't super important right now, but it will be useful when updating the chart later
+-}
+type alias Model =
+    { data : List Float
+    , labels : List String
+    }
+
+{-| Initialise the model with some basic data and examples
+-}
+init : Model
+init =
+    { data = [ 4, 8, 15, 16, 23, 42 ]
+    , labels = [ "One", "Two", "Three", "Four", "Five", "Six" ]
+    }
+
+
+{-| Build a Chartjs data object from our model
+First we need a Data container which has some basic chart information
+Then we need to make the dataset for our bar chart
+This can be done easily using pipe operators instead of record update syntax
+-}
+data : Model -> ChartData.Data
+data model =
+    let 
+        dataset =
+            PieData.defaultPieFromData "Example Chart" model.data
+                |> PieData.setBackgroundColor (ChartCommon.PerPoint ChartCommon.defaultColors)
+    in
+    ChartData.dataFromLabels model.labels
+        |> ChartData.addDataset (ChartData.PieData dataset)
+
+legendConfig : ChartLegend.Legend
+legendConfig =
+    ChartLegend.defaultLegend
+        |> ChartLegend.setDisplay True
+        |> ChartLegend.setPosition ChartCommon.Left
+
+titleConfig : ChartTitle.Title
+titleConfig =
+    ChartTitle.defaultTitle
+        |> ChartTitle.setAlign ChartCommon.End
+        |> ChartTitle.setPosition ChartCommon.Bottom
+        |> ChartTitle.setText "Fancy Title"
+        |> ChartTitle.setColor Color.black
+        |> ChartTitle.setFont (ChartFont.create "sans-serif" 32)
+        |> ChartTitle.setDisplay True
+
+chartOptions : ChartOptions.Options
+chartOptions =
+    ChartOptions.defaultOptions
+        |> ChartOptions.setLegend legendConfig
+        |> ChartOptions.setTitle titleConfig
+
+{-| Build the full chart configuration from our model
+-}
+chartConfig : Model -> Chart.Chart
+chartConfig model =
+    Chart.defaultChart Chart.Pie
+        |> Chart.setData (data model)
+        |> Chart.setOptions chartOptions
+
+
+{-| Display the chart using the chart config
+If we wanted to resize the chart or add other attributes, we can do that here
+-}
+view : Model -> Html msg
+view model =
+    Chart.chart [] (chartConfig model)
+
+
+{-| There's nothing to update in this example, so this is a dummy update function
+-}
+update : Model -> msg -> Model
+update model _ =
+    model
+
+
+{-| Simple browser sandbox to run this example
+-}
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
